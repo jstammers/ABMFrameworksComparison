a47c6ad04ef20c27a3870b399eaa02f4dbc779ae
jstammers <jimmy.stammers@gmail.com>
2026-04-24 23:31:50 +0100

    revert CI setup

--- .github/setup.sh
+++ .github/setup.sh
@@ -28,7 +28,37 @@ if [[ "$ARCH" == "x86_64" || "$ARCH" == "amd64" ]]; then
   JULIA_ARCH_FILE="x86_64"
 elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
   JULIA_ARCH_PATH="aarch64"
-  JULIA_ARCH_FILE="aarch64"
+# fetch update software list
+sudo apt-get update
+
+# give permissions
+sudo chmod a+rwx ./
+sudo chmod -R 777 ./
+
+# install java
+sudo apt install default-jre-headless
+sudo apt install default-jdk-headless
+
+# install julia
+sudo wget https://julialang-s3.julialang.org/bin/linux/x64/1.10/julia-1.11.5-linux-x86_64.tar.gz
+sudo tar zxvf julia-1.11.5-linux-x86_64.tar.gz
+export PATH=$PATH:$(pwd)"/julia-1.11.5/bin"
+printf "\nexport PATH=\"\$PATH:"$(pwd)"/julia-1.11.5/bin\"" >> ~/.bashrc
+
+# install agents
+julia --project=@. -e 'using Pkg; Pkg.instantiate()'
+
+# install mesa
+sudo apt install python3-pip
+pip install mesa==3.2.0 mesa-frames==0.1.0a0
+
+# install netlogo
+sudo wget https://downloads.netlogo.org/6.4.0/NetLogo-6.4.0-64.tgz
+sudo tar -xzf NetLogo-6.4.0-64.tgz
+sudo mv "NetLogo-6.4.0-64" netlogo
+
+# install bc tool
+sudo apt install bc  JULIA_ARCH_FILE="aarch64"
 else
   echo "Unsupported architecture for Julia install: $ARCH" >&2
   exit 1
